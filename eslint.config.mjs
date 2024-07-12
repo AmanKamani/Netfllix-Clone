afd703a04583536e39959d5d0d97c571616c2cea
AmanKamani <kamaniamanak@gmail.com>
2024-07-13 01:58:47 +0530

    feat: configured eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    rules: {
+      "react/display-name": "off"
+    }
+  }
+];
