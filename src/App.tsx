2f72cb0a30ab973a105308057b4a575e76b73370
AmanKamani <kamaniamanak@gmail.com>
2024-07-13 23:41:55 +0530

    refactor: Renamed accoridan container to faqs container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import AccoridanContainer from './containers/accordian';
+import { FaqsContainer } from './containers/faqs';
 import FooterContainer from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
 
 const App = () => {
   return (<>
     <JumbotronContainer />
-    <AccoridanContainer />
+    <FaqsContainer />
     <FooterContainer />
   </>);
 }
